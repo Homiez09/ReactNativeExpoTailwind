081a16bc65a184ef4273d0bdbc858698504e08a2
HomieZ09 <phumsoen123@gmail.com>
2024-09-15 01:28:55 +0700

    add tailwind

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,6 +1,8 @@
 import { ScrollViewStyleReset } from 'expo-router/html';
 import { type PropsWithChildren } from 'react';
 
+// import "@/global.css";
+
 /**
  * This file is web-only and used to configure the root HTML for every web page during static rendering.
  * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
@@ -34,6 +36,6 @@ body {
 }
 @media (prefers-color-scheme: dark) {
   body {
-    background-color: #000;
+    background-color: #fff;
   }
 }`;
